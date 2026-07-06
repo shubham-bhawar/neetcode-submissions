c8d4ea61ea99bc95510c8850c33d2f40d1e0005d
shubham-bhawar <64727723+shubham-bhawar@users.noreply.github.com>
2026-07-06 21:38:16 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int maxBanana = 0;
+        for (int i = 0; i < piles.length; i++) {
+            maxBanana = Math.max(maxBanana, piles[i]);
+        }
+
+        int start = 1;
+        int end = maxBanana;
+        int ans = 0;
+        while (start <= end) {
+            int hrs=0;
+            int mid = (start + end) / 2;
+
+            
+
+            for (int i = 0; i < piles.length; i++) {
+                hrs += (int) Math.ceil((double)piles[i]/mid);
+                System.out.print("hrs :- "+hrs +" ***");
+            }
+
+            System.out.println("start :- "+ start +"end :- "+end +"mid :-"+ mid + "hrs :-"+hrs);
+
+            if(hrs <= h){
+                end = mid -1 ;
+                ans = mid;
+            }else{
+                start = mid+1;
+            }
+        }
+
+        return ans;
+    }
+}
